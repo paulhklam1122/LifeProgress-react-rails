914e1d52f86e8fd1de933ba04fce0f64b59c924e
Paul Lam <Paul@Paul-Lams-MacBook-Pro.local>
2016-10-15 14:24:06 -0700

    added gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
+//= require bootstrap-sprockets
+//= require react
+//= require react_ujs
+//= require components
